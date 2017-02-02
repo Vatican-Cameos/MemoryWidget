33e44cb70d5ebea75e96483c59911e078d7e7a9b
Pavan <pavanvenkatesh94@gmail.com>
2017-02-02 18:14:34 +0530

    implemented a widget which monitors internal storage.

--- app/src/main/java/in/goodiebag/memorywidget/MemoryWidget.java
+++ app/src/main/java/in/goodiebag/memorywidget/MemoryWidget.java
@@ -1,9 +1,25 @@
 package in.goodiebag.memorywidget;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Path;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.StatFs;
+import android.util.Log;
 import android.widget.RemoteViews;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Random;
+
+import static android.provider.Telephony.ThreadsColumns.ERROR;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,7 +29,7 @@ public class MemoryWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        CharSequence widgetText = "Internal : " +getAvailableInternalMemorySize() + "/" + getTotalInternalMemorySize() + " \n" + " External : " + getAvailableExternalMemorySize() + "/" + getTotalExternalMemorySize();
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.memory_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
@@ -39,5 +55,69 @@ public class MemoryWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+
+    public static String getAvailableInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();
+        long availableBlocks = stat.getAvailableBlocks();
+        return formatSize(availableBlocks * blockSize);
+    }
+
+    public static String getTotalInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();
+        long totalBlocks = stat.getBlockCount();
+        return formatSize(totalBlocks * blockSize);
+    }
+
+    public static String getAvailableExternalMemorySize() {
+        if (externalMemoryAvailable()) {
+            //TODO :: Find a method to find external storage space
+            return null;
+        } else {
+            return ERROR;
+        }
+    }
+
+    public static String getTotalExternalMemorySize() {
+        if (externalMemoryAvailable()) {
+            //TODO :: Find a method to find external storage space
+            return null;
+        } else {
+            return ERROR;
+        }
+    }
+
+    public static boolean externalMemoryAvailable() {
+        return android.os.Environment.getExternalStorageState().equals(
+                android.os.Environment.MEDIA_MOUNTED);
+    }
+
+    public static String formatSize(long size) {
+        String suffix = null;
+
+        if (size >= 1024) {
+            suffix = "KB";
+            size /= 1024;
+            if (size >= 1024) {
+                suffix = "MB";
+                size /= 1024;
+            }
+        }
+        StringBuilder resultBuffer = new StringBuilder(Long.toString(size));
+
+        int commaOffset = resultBuffer.length() - 3;
+        while (commaOffset > 0) {
+            resultBuffer.insert(commaOffset, ',');
+            commaOffset -= 3;
+        }
+
+        if (suffix != null) resultBuffer.append(suffix);
+        return resultBuffer.toString();
+    }
+
 }
 
